f9bd510d59621b1dd96d08d4c82545d4095048fe
Paul Rosero <paul_rosero@yahoo.com>
2019-05-08 01:00:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,9 @@
-class Song
+cclass Song
   attr_accessor :name, :artist, :genre
 
   @@count = 0
+  @@artists = []
   @@genres = []
-  @@gartists = []
 
   def initialize(name, artist, genre)
     @name = name
@@ -16,22 +16,6 @@ class Song
 
   def self.count
     @@count
-  end
-
-  def self.genres
-    @@genres.uniq
-  end
-
-  def self.genre_count
-    genre_count = {}
-    @@genres.each do |genre|
-      if genre_count[genre]
-        genre_count[genre] += 1
-      else
-        genre_count[genre] = 1
-      end
-    end
-    genre_count
   end
 
   def self.artists
@@ -48,5 +32,21 @@ class Song
       end
     end
     artist_count
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
   end
 end
